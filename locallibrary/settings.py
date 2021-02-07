57685aa640df045414e7ce10350db13d7e4cfb21
akaeaa <akaeaa@gmail.com>
2021-02-07 16:54:13 -0700

    Update allowed hosts to site and development server url

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 #set DJANGO_DEBUG=False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['powerful-temple-23910.herokuapp.com','127.0.0.1']
 
 
 # Application definition
